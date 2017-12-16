faf114dfcb57a6891b66138d02d89dd5dae2c7a1
Andy Edwards <jedwards@fastmail.com>
2017-12-16 01:09:08 -0600

    refactor: delete more dead code

--- src/universal/util/urlUtil.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import parseURL from 'url-parse'
-
-export function toWebSocketURL(hostURL: string, servicePath: string): string {
-  const { protocol, host } = parseURL(hostURL)
-  let wsProtocol = 'https:' === protocol || 'wss:' === protocol ? 'wss:' : 'ws:'
-  return `${wsProtocol}//${host}${servicePath}`
-}
