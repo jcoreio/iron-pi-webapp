cd529d70245fd0a26d8651cbad711f844c38d14c
Andy Edwards <jedwards@fastmail.com>
2018-01-17 14:24:15 -0600

    test: fix bug in getText

--- test/selenium/util/getText.js
+++ test/selenium/util/getText.js
@@ -2,7 +2,7 @@
 
 export default async function getText(selector: string): Promise<string | Array<string>> {
   const {value: elements} = await browser.elements(selector)
-  if (elements.length === 1) return await browser.elementIdText(elements[0].ELEMENT)
+  if (elements.length === 1) return (await browser.elementIdText(elements[0].ELEMENT)).value
   const text = []
   for (let i = 0; i < elements.length; i++) {
     text.push((await browser.elementIdText(elements[i].ELEMENT)).value)
