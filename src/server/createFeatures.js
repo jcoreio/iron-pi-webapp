1be44ae52a9c6153d043ba4c0655213aac087244
Andy Edwards <jedwards@fastmail.com>
2018-02-14 14:05:18 -0600

    chore: temporarily add localio feature

--- src/server/createFeatures.js
+++ src/server/createFeatures.js
@@ -1,9 +1,11 @@
 // @flow
 
 import type {ServerFeature} from './ServerFeature'
+import {createFeature as createLocalIOFeature} from './localio/LocalIOFeature'
 
 export default async function createFeatures(): Promise<Array<ServerFeature>> {
   return [
+    createLocalIOFeature(),
   ]
 }
 
