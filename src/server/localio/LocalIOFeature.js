2da69f1edb519f908b45ecf4c00d9d8ff7894eb1
Andy Edwards <jedwards@fastmail.com>
2018-02-14 14:48:51 -0600

    feat(LocalIOFeature): add sequelize models and graphql queries

--- src/server/localio/LocalIOFeature.js
+++ src/server/localio/LocalIOFeature.js
@@ -3,13 +3,58 @@
 import path from 'path'
 import glob from 'glob'
 import promisify from 'es6-promisify'
+import * as graphql from 'graphql'
+import type Sequelize from 'sequelize'
 import EventEmitter from '@jcoreio/typed-event-emitter'
+import {attributeFields, defaultArgs, resolver} from 'graphql-sequelize'
+import LocalIOChannel from './LocalIOChannel'
+
+import defaultInputType from '../graphql/types/defaultInputType'
+import type {Context} from '../graphql/Context'
 import type {FeatureEmittedEvents} from '../data-router/PluginTypes'
 import type {ServerFeature} from '../ServerFeature'
+import requireUserId from '../graphql/requireUserId'
 
 export class LocalIOFeature extends EventEmitter<FeatureEmittedEvents> {
   async getMigrations(): Promise<Array<string>> {
     return promisify(glob)(path.join(__dirname, 'migrations', '*.js'))
+  }
+  addSequelizeModels({sequelize}: {sequelize: Sequelize}) {
+    LocalIOChannel.initAttributes({sequelize})
+    LocalIOChannel.initAssociations()
+  }
+  addTypes({types, inputTypes, attributeFieldsCache}: {
+    types: {[name: string]: graphql.GraphQLOutputType},
+    inputTypes: {[name: string]: graphql.GraphQLInputType},
+    attributeFieldsCache: Object,
+  }) {
+    types[LocalIOChannel.name] = new graphql.GraphQLObjectType({
+      name: LocalIOChannel.name,
+      fields: () => ({
+        ...attributeFields(LocalIOChannel, {cache: attributeFieldsCache})
+      }),
+    })
+    inputTypes.LocalIOChannel = defaultInputType(LocalIOChannel, {cache: attributeFieldsCache})
+  }
+  addQueryFields({types, queryFields}: {
+    types: {[name: string]: graphql.GraphQLOutputType},
+    queryFields: {[name: string]: graphql.GraphQLFieldConfig<any, Context>},
+  }) {
+    for (let model of [LocalIOChannel]) {
+      const type = types[model.name]
+      if (!type) continue
+      const {options} = model
+      queryFields[options.name.singular] = {
+        type,
+        args: defaultArgs(model),
+        resolve: resolver(model, {before: requireUserId}),
+      }
+      queryFields[options.name.plural] = {
+        type: new graphql.GraphQLList(type),
+        args: defaultArgs(model),
+        resolve: resolver(model, {before: requireUserId}),
+      }
+    }
   }
 }
 
