ae9706de837d3639365fe58bd7c71dd5963e7ab2
Andy Edwards <jedwards@fastmail.com>
2018-02-14 14:03:33 -0600

    fix: fix flow error

--- src/server/getFeatures.js
+++ src/server/getFeatures.js
@@ -1,11 +1,9 @@
 // @flow
 
 import type {ServerFeature} from './ServerFeature'
-import {createFeature as createLocalIOFeature} from './localio/LocalIOFeature'
 
 export default async function getFeatures(): Promise<Array<ServerFeature>> {
   return [
-    createLocalIOFeature(),
   ]
 }
 
