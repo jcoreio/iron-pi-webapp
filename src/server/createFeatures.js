e4f34566382f7c0c966c365da20b4b440588330f
Andy Edwards <jedwards@fastmail.com>
2018-02-20 13:26:35 -0600

    feat: add MQTT feature to createFeatures

--- src/server/createFeatures.js
+++ src/server/createFeatures.js
@@ -2,10 +2,12 @@
 
 import type {ServerFeature} from './ServerFeature'
 import {createFeature as createLocalIOFeature} from './localio/LocalIOFeature'
+import {createFeature as createMQTTFeature} from './mqtt/MQTTFeature'
 
 export default async function createFeatures(): Promise<Array<ServerFeature>> {
   return [
     createLocalIOFeature(),
+    createMQTTFeature(),
   ]
 }
 
