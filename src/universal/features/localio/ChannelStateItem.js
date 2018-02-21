06a2a2bee2840519d87bbce1c8e9387b60fd7a8c
Andy Edwards <jedwards@fastmail.com>
2018-02-21 17:04:51 -0600

    fix(ChannelStateItem): restore display name

--- src/universal/features/localio/ChannelStateItem.js
+++ src/universal/features/localio/ChannelStateItem.js
@@ -53,17 +53,15 @@ export type ChannelStateProps = {
   classes: Classes,
 }
 
-const ChannelStateItem = withStyles(channelStateStyles, {withTheme: true})(
-  ({channel, classes}: ChannelStateProps): React.Node => (
-    <SidebarItem component={NavLink} to={channelForm(channel.id + 1)} data-component="ChannelStateItem">
-      <SidebarItemText data-test-name="id" className={classes.id} primary={String(channel.id + 1)} />
-      <SidebarItemText data-test-name="name" disableTypography primary={channel.name} className={classes.name} />
-      <ListItemSecondaryAction className={classes.secondaryAction}>
-        <ChannelStateIcon channel={channel} />
-      </ListItemSecondaryAction>
-    </SidebarItem>
-  )
+const ChannelStateItem = ({channel, classes}: ChannelStateProps): React.Node => (
+  <SidebarItem component={NavLink} to={channelForm(channel.id + 1)} data-component="ChannelStateItem">
+    <SidebarItemText data-test-name="id" className={classes.id} primary={String(channel.id + 1)} />
+    <SidebarItemText data-test-name="name" disableTypography primary={channel.name} className={classes.name} />
+    <ListItemSecondaryAction className={classes.secondaryAction}>
+      <ChannelStateIcon channel={channel} />
+    </ListItemSecondaryAction>
+  </SidebarItem>
 )
 
-export default ChannelStateItem
+export default withStyles(channelStateStyles, {withTheme: true})(ChannelStateItem)
 
