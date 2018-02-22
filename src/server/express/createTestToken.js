2f3d5a815936d1ca5f88e001128b284f6e969bf3
Andy Edwards <jedwards@fastmail.com>
2018-02-22 16:21:25 -0600

    fix(createTestToken): fix scopes access

--- src/server/express/createTestToken.js
+++ src/server/express/createTestToken.js
@@ -7,13 +7,13 @@ import type {DecodedToken} from '../auth/DecodedToken'
 
 async function createTestToken(req: $Request, res: $Response): Promise<void> {
   const {userId, scopes}: DecodedToken = (req: any)
-  if (!Array.isArray(scopes) || scopes.indexOf('test:create:token')) {
+  if (!scopes.has('test:create:token')) {
     res.status(403).json({error: 'forbidden'})
     return
   }
 
   const {expiresIn} = req.body
-  const token = await createToken({userId, scopes, expiresIn})
+  const token = await createToken({userId, scopes: [...scopes], expiresIn})
   res.status(200).json({token})
 }
 
