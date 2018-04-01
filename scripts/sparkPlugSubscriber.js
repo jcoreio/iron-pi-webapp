33961511937b998401eb721c85cecfa17d6c0601
Jason McLaurin <jrmclaurin@users.noreply.github.com>
2018-03-31 23:02:53 -0500

    feat: print out metadata in SparkPlug subscriber / logger

--- scripts/sparkPlugSubscriber.js
+++ scripts/sparkPlugSubscriber.js
@@ -13,6 +13,14 @@ const client = sparkplug.newClient({
 client.on('error', err => console.error(`MQTT error: ${err.stack || err}`))
 client.on('appMessage', msg => {
   const {groupId, edgeNode, payload: {metrics}} = msg
-  const strMetrics = metrics.map(metric => `  ${metric.name}: ${metric.value}`).join('\n')
+  const strMetrics = metrics.map(metric => {
+    const {properties} = metric
+    if (properties) {
+      const propValue = propName => properties[propName].value
+      return `  ${metric.name}: name: ${propValue('longName')} range: ${propValue('min')} - ${propValue('max')} units: ${propValue('units')} value: ${metric.value}`
+    } else {
+      return `  ${metric.name}: ${metric.value}`
+    }
+  }).join('\n')
   console.log(`group: ${groupId} node: ${edgeNode}\n${strMetrics}`)
 })
