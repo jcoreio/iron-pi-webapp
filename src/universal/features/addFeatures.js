3a0eb39730043c925574d504ceea11fa3044a635
Jason McLaurin <jrmclaurin@users.noreply.github.com>
2018-04-06 12:24:22 -0500

    fix(securityView): fix incorrect case in SecurityViewFeature include

--- src/universal/features/addFeatures.js
+++ src/universal/features/addFeatures.js
@@ -5,7 +5,7 @@ import {addFeature} from 'redux-features'
 import LocalIOFeature, {FEATURE_ID as LOCAL_IO_FEATURE_ID} from './localio/LocalIOFeature'
 import MQTTFeature, {FEATURE_ID as MQTT_FEATURE_ID} from './mqtt/MQTTFeature'
 import NetworkSettingsFeature, {FEATURE_ID as NETWORK_SETTINGS_FEATURE_ID} from './networkSettings/NetworkSettingsFeature'
-import SecurityViewFeature, {FEATURE_ID as SECURITY_VIEW_FEATURE_ID} from './SecurityView/SecurityViewFeature'
+import SecurityViewFeature, {FEATURE_ID as SECURITY_VIEW_FEATURE_ID} from './securityView/SecurityViewFeature'
 
 export default function addFeatures(store: Store) {
   store.dispatch(addFeature(LOCAL_IO_FEATURE_ID, LocalIOFeature))
