3ce720b3ce436a99a28f15cf531214b5c3747e51
Jason McLaurin <jrmclaurin@users.noreply.github.com>
2018-04-06 19:21:19 -0500

    fix: block SSH and system password operations unless MANAGE_OS_SECURITY env var is set

--- src/server/device/SSHHandler.js
+++ src/server/device/SSHHandler.js
@@ -5,34 +5,43 @@ import promisify from 'es6-promisify'
 
 const exec = promisify(child_process.exec)
 
+// Don't attempt to enable / disable SSH or set passwords in desktop dev environments.
+const manageOSSecurity = () => !!process.env.MANAGE_OS_SECURITY
+
 export interface SSHHandler {
-  isSSHEnabled(): Promise<boolean>;
+  isSSHEnabled(): Promise<?boolean>;
   setSSHEnabled(enabled: boolean): Promise<void>;
   setSystemPassword(password: string): Promise<void>;
 }
 
 export default class DeviceSSHHandler implements SSHHandler {
-  async isSSHEnabled(): Promise<boolean> {
-    try {
-      const status = await exec('systemctl is-enabled ssh')
-      return typeof status === 'string' && 'enabled' === status.trim()
-    } catch (err) {
-      // When ssh is not enabled, the command returns 1
-      if (err.code === 1) return false
-      throw err
+  async isSSHEnabled(): Promise<?boolean> {
+    if (manageOSSecurity()) {
+      try {
+        const status = await exec('systemctl is-enabled ssh')
+        return typeof status === 'string' && 'enabled' === status.trim()
+      } catch (err) {
+        // When ssh is not enabled, the command returns 1
+        if (err.code === 1) return false
+        throw err
+      }
+    } else {
+      return null
     }
   }
 
   async setSSHEnabled(enabled: boolean): Promise<void> {
-    if (enabled !== await this.isSSHEnabled()) {
+    if (manageOSSecurity() && enabled !== await this.isSSHEnabled()) {
       await exec(`systemctl ${enabled ? 'enable' : 'disable'} ssh`)
       await exec(`systemctl ${enabled ? 'start' : 'stop'} ssh`)
     }
   }
 
   async setSystemPassword(password: string): Promise<void> {
-    if (!password || typeof password !== 'string') throw new Error('password must be a non empty string')
-    await exec(`echo "pi:${password}" | chpasswd`)
+    if (manageOSSecurity()) {
+      if (!password || typeof password !== 'string') throw new Error('password must be a non empty string')
+      await exec(`echo "pi:${password}" | chpasswd`)
+    }
   }
 }
 
--- src/server/graphql/mutation/changePassword.js
+++ src/server/graphql/mutation/changePassword.js
@@ -62,7 +62,8 @@ export default function changePassword(): GraphQLFieldConfig<any, GraphQLContext
       if (!user) throw new Error('User not found')
       try {
         await user.update({password: newPassword, passwordHasBeenSet: true}, {individualHooks: true})
-        if (process.env.SET_SYSTEM_PASSWORD) await sshHandler.setSystemPassword(newPassword)
+        // This call will not actually set the system password unless process.env.MANAGE_OS_SECURITY is truthy
+        await sshHandler.setSystemPassword(newPassword)
       } catch (error) {
         if (error instanceof ValidationError) {
           const passwordItem = error.errors.find(item => item.path === 'password')
