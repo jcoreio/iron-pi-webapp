2fbc608705a6f386fd1c80666ccbf646b5923953
Jason McLaurin <jrmclaurin@users.noreply.github.com>
2019-06-29 22:02:33 -0500

    feat: automate installing node.js, yarn, and git on an Iron Pi

---
+++ os/bootstrap.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+NODE_VERSION=10.16.0
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Add apt-get sources for yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update
+
+echo "installing node.js..."
+NODE_FULL_VERSION=node-v${NODE_VERSION}-linux-armv7l
+TAR_FILE=${NODE_FULL_VERSION}.tar.xz
+wget https://nodejs.org/dist/v${NODE_VERSION}/${TAR_FILE}
+tar xf ${TAR_FILE}
+rm ${TAR_FILE}
+cd ${NODE_FULL_VERSION}
+sudo cp -R bin include lib share /usr/local
+cd ..
+rm -rf ${NODE_FULL_VERSION}
+
+echo "installing yarn..."
+sudo apt-get install -y --no-install-recommends yarn
+
+sudo apt-get install -y git
